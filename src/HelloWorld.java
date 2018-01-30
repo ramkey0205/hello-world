c4029837e661c0f81d989a73f2960c52a4bfd083
Administrator <Administrator@SC-201801121136>
2018-01-30 16:40:59 +0900

    install commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("java");
+		System.out.println("이거 되나 한번보자");
+		System.out.println("오랜만에 자바시작했다");
+	}
+}
